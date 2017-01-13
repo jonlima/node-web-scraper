05f952e1784636c8c3611bb4097a7dd812a0476c
Jonatha Lima <jonatha.lima14@gmail.com>
2017-01-12 21:51:08 -0300

    request

--- scraper-v1.js
+++ scraper-v1.js
@@ -8,6 +8,32 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 var app = express();
 
+app.get('/scrape', function(req, res){
+	var url = 'http://www.imdb.com/title/tt4425200';
+
+	request(url, function(error, response, html) {
+		if(!error) {
+			var $ = cheerio.load(html);
+			var title;
+			var release;
+			var rating;
+			
+			var json = {
+				title: "",
+				release: "",
+				rating: ""
+			};
+
+			$('.originalTitle').filter(function() {
+				var data = $(this);
+				title = data.children().first().text();
+				
+				json.title = title;
+			});
+		}
+	});
+});
+
 app.listen('7000', function() {
 	console.log('App listening on port 7000');
 })
